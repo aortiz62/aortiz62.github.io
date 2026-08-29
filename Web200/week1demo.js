1f6cb133b859d10f7f02b20d34bb323c13336e3b
aortiz62 <142848329+aortiz62@users.noreply.github.com>
2026-08-29 13:38:59 -0500

    Add interactive button and alert in week1demo.js

---
+++ Web200/week1demo.js
@@ -0,0 +1,12 @@
+const button = document.createElement("button");
+button.type = "button";
+button.textContent = "But do I really?...";
+button.onclick = () => document.write("yes i definitely do");
+document.body.appendChild(button);
+button.animate
+
+console.log("oh my god this is cool");
+
+window.alert("Hello there!");
+
+document.getElementById("demo").innerText = "I love the smell of fresh coffee being brewed";
